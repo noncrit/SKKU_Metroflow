6991ceb9e717909f55ce3f813be37a862cc3e528
seok_soldesk <kimksj09996@gmail.com>
2024-10-01 14:05:11 +0900

    RENEW : 더미 파일 삭제중

--- src/main/java/com/metroflow/repository/FavoriteListRepository.java
+++ src/main/java/com/metroflow/repository/FavoriteListRepository.java
@@ -2,6 +2,6 @@ package com.metroflow.repository;
 
 import com.metroflow.model.dto.FavoriteList;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//변경사항 생성용 주석
 public interface FavoriteListRepository extends JpaRepository<FavoriteList, Long> {
 }
