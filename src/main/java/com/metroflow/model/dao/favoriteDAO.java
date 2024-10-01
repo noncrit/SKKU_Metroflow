951cc98b329c8a6eb081d8f303a7384a0eef23ce
seok_soldesk <kimksj09996@gmail.com>
2024-10-01 13:41:38 +0900

    FEAT : 즐겨찾기 initial commit

--- src/main/java/com/metroflow/model/dao/favoriteDAO.java
+++ src/main/java/com/metroflow/model/dao/favoriteDAO.java
@@ -2,7 +2,11 @@ package com.metroflow.model.dao;
 
 import com.metroflow.repository.FavoriteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class favoriteDAO {
 
     private final FavoriteRepository favoriteRepository;
@@ -11,4 +15,5 @@ public class favoriteDAO {
     public favoriteDAO(FavoriteRepository favoriteRepository) {
         this.favoriteRepository = favoriteRepository;
     }
+
 }
