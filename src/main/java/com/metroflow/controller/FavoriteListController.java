69f03aa5308f3514825541c40ffdc9f4e446b741
Seokjin <kimksj09996@gmail.com>
2024-10-09 13:01:00 +0900

    FEAT: 즐겨찾기 페이징 처리, 시간값 예외처리 구현 - 현재 시간, 날짜 미 설정시 자동으로 처리되도록 구현 - (중요) SubwayMapService 기능 추가 isValidTimeFormat : dto에 맞는 입력값인지 체크하는 기능 (h0330 같은 없는 값 체크용)

--- src/main/java/com/metroflow/controller/FavoriteListController.java
+++ src/main/java/com/metroflow/controller/FavoriteListController.java
@@ -12,13 +12,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Controller
@@ -29,14 +29,13 @@ public class FavoriteListController {
     private FavoriteListService favoriteListService;
     @Autowired
     private UserService userService;
-
     @Autowired
     private SubwayMapService subwayMapService;
-
     @Autowired
     private IsHolidaysService isHolidaysService;
+    @Autowired
+    private SearchService searchService;
 
-    private final UserService USERSERVICE;
     private final NoticeBoardRepository NOTICEBOARDREPOSITORY;
     private final NoticeBoardService NOTICEBOARDSERVICE;
 
@@ -77,29 +76,28 @@ public class FavoriteListController {
 
     // 메뉴바에서 a 태그 버튼 눌렀을 경우
     @GetMapping("/goFavoriteList")
-    public String goFavoriteList(Model model){
+    public String goFavoriteList(@RequestParam(defaultValue = "0") int page, Model model){
 
         String favoriteList_user_id = userService.getUserObject().getUserId();
 
         if(favoriteList_user_id != null){
-            int page = 0;
-            // 6개 레코드가 완전한 데이터 1개 !!
-            int size = 24;
+            // 4개 레코드가 완전한 데이터 1개 !!
+            int size = 32;
             Pageable pageable = PageRequest.of(page, size);
             Page<FavoriteListPageDto> favoriteListPage = favoriteListService.getFavoriteListByUserId(favoriteList_user_id,pageable);
 
-                        favoriteListPage.getContent().forEach(item -> {
-//                System.out.println("User ID: " + item.getUserId());
-                System.out.println("Station ID: " + item.getStationId());
-                System.out.println("Station Line: " + item.getStationLine());
-                System.out.println("Station Name: " + item.getStationName());
-                System.out.println("Direction Type: " + item.getDirectionType());
-                System.out.println("Day Type: " + item.getDayType());
-//                System.out.println("Current Time Congestion : "+ item.getSubwayTime().getH1530());
-            });
+//                        favoriteListPage.getContent().forEach(item -> {
+////                System.out.println("User ID: " + item.getUserId());
+//                System.out.println("Station ID: " + item.getStationId());
+//                System.out.println("Station Line: " + item.getStationLine());
+//                System.out.println("Station Name: " + item.getStationName());
+//                System.out.println("Direction Type: " + item.getDirectionType());
+//                System.out.println("Day Type: " + item.getDayType());
+////                System.out.println("Current Time Congestion : "+ item.getSubwayTime().getH1530());
+//            });
                         
             // setTime을 인덱스처럼 사용할 예정 (h0530 같은 값으로)
-            String setTime = subwayMapService.getCurrentTimeColumn();
+            String setTime = subwayMapService.isValidTimeFormat(subwayMapService.getCurrentTimeColumn());
             String dayType = subwayMapService.getCurrentDayType();
 
             // ampm, 시, 분 을 가져오는 기능
@@ -111,9 +109,12 @@ public class FavoriteListController {
 
             // station_id를 이용해 매핑 처리해서 station_id 하위 항목으로 상선, 하선 데이터가 들어가도록 조정
             // 그룹화 (평일, 휴일 기준으로 매칭되는 데이터만 매핑시킴)
-            Map<Long, List<FavoriteListPageDto>> groupedFavorites = favoriteListPage.getContent().stream()
+            Map<Long, List<FavoriteListPageDto>> raw_groupedFavorites = favoriteListPage.getContent().stream()
                     .filter(item -> item.getDayType().equals(dayType)) // dayType이 일치하는 항목만 필터링
                     .collect(Collectors.groupingBy(FavoriteListPageDto::getStationId));
+
+            // station_id 순으로 정렬(오름차순)
+            Map<Long, List<FavoriteListPageDto>> groupedFavorites = new TreeMap<>(raw_groupedFavorites);
 
             // 모델에 데이터 추가
             model.addAttribute("setTime", setTime);
@@ -124,7 +125,7 @@ public class FavoriteListController {
 
         }
 
-        model.addAttribute("sessionUser", USERSERVICE.getUserObject());
+        model.addAttribute("sessionUser", userService.getUserObject());
 
         List<NoticeBoard> notices = NOTICEBOARDREPOSITORY.findAll(Sort.by(Sort.Direction.DESC, "boardNo"));
         for (NoticeBoard notice : notices) {
@@ -136,4 +137,98 @@ public class FavoriteListController {
 
         return "favoriteList/favoriteList";
     }
+
+    @GetMapping("/goFavoriteListWithSetTime")
+    public String goFavoriteListWithSetTime(@RequestParam(required = false) String year,
+                                            @RequestParam(required = false) String month,
+                                            @RequestParam(required = false) String day,
+                                            @RequestParam(required = false) String ampm,
+                                            @RequestParam(required = false) String hour,
+                                            @RequestParam(required = false) String minute,
+                                            @RequestParam(defaultValue = "0") int page,
+                                            Model model){
+
+        // 날짜 미 선택시 현재 날짜 기준 표시
+        if (year == null || year.isEmpty()) {
+            year = String.valueOf(LocalDate.now().getYear());
+        }
+        if (month == null || month.isEmpty()) {
+            month = String.format("%02d", LocalDate.now().getMonthValue());
+        }
+        if (day == null || day.isEmpty()) {
+            day = String.format("%02d", LocalDate.now().getDayOfMonth());
+        }
+
+        // 시간 미선택 시 현재 시간 기준 처리
+        if (ampm == null || ampm.isEmpty()) {
+            LocalDateTime now = LocalDateTime.now();
+            ampm = now.getHour() < 12 ? "AM" : "PM";
+        }
+        if (hour == null || hour.isEmpty()) {
+            hour = String.format("%02d", LocalDateTime.now().getHour() % 12); // 12시간제로 2자리 포맷
+            if (hour.equals("00")) {
+                hour = "12"; // 0시를 12시로 변환
+            }
+        }
+        if (minute == null || minute.isEmpty()) {
+            minute = String.format("%02d", LocalDateTime.now().getMinute()); // 현재 분
+        }
+
+        String targetDate = isHolidaysService.classifyDate(LocalDate.parse(year + "-" + month + "-" + day));
+
+        String favoriteList_user_id = userService.getUserObject().getUserId();
+
+        if(favoriteList_user_id != null){
+            // 4개 레코드가 완전한 데이터 1개 !!
+            int size = 32;
+            Pageable pageable = PageRequest.of(page, size);
+            Page<FavoriteListPageDto> favoriteListPage = favoriteListService.getFavoriteListByUserId(favoriteList_user_id,pageable);
+
+            // setTime을 인덱스처럼 사용할 예정 (h0530 같은 값으로)
+            // 사용자가 설정한 시간으로 바꿔줘야함
+            // 정확한 시간 포맷인지 검사후 넘기기
+            String setTime = subwayMapService.isValidTimeFormat(searchService.getInputColumn(ampm, hour, minute));
+
+            // 위에서 설정한 targetDate로 설정
+            String dayType = targetDate;
+
+            // ampm, 시, 분 을 가져오는 기능
+            // 페이지 최초 접근시에는 현재 기준으로 설정
+            model.addAttribute("amPm",ampm);
+            model.addAttribute("hour",hour);
+            model.addAttribute("minute",minute);
+
+            // station_id를 이용해 매핑 처리해서 station_id 하위 항목으로 상선, 하선 데이터가 들어가도록 조정
+            // 그룹화 (평일, 휴일 기준으로 매칭되는 데이터만 매핑시킴)
+            Map<Long, List<FavoriteListPageDto>> raw_groupedFavorites = favoriteListPage.getContent().stream()
+                    .filter(item -> item.getDayType().equals(dayType)) // dayType이 일치하는 항목만 필터링
+                    .collect(Collectors.groupingBy(FavoriteListPageDto::getStationId));
+
+            // station_id 순으로 정렬(오름차순)
+            Map<Long, List<FavoriteListPageDto>> groupedFavorites = new TreeMap<>(raw_groupedFavorites);
+
+            // 모델에 데이터 추가
+            model.addAttribute("setTime", setTime);
+            model.addAttribute("groupedFavorites", groupedFavorites);   // 매핑시킨 데이터
+            model.addAttribute("favoriteList", favoriteListPage.getContent()); // 페이지의 내용
+            model.addAttribute("totalPages", favoriteListPage.getTotalPages()); // 총 페이지 수
+            model.addAttribute("currentPage", favoriteListPage.getNumber()); // 현재 페이지 번호
+
+        }
+
+        model.addAttribute("sessionUser", userService.getUserObject());
+
+        List<NoticeBoard> notices = NOTICEBOARDREPOSITORY.findAll(Sort.by(Sort.Direction.DESC, "boardNo"));
+        for (NoticeBoard notice : notices) {
+            String contents = NOTICEBOARDSERVICE.modifyBoardText(notice.getBoardNo());
+            notice.getBoard().setBoardText(contents);
+        }
+        model.addAttribute("notices", notices);
+
+
+        return "favoriteList/favoriteList";
+
+
+    }
+
 }
--- src/main/java/com/metroflow/model/service/SubwayMapService.java
+++ src/main/java/com/metroflow/model/service/SubwayMapService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class SubwayMapService {
@@ -20,6 +22,46 @@ public class SubwayMapService {
     @Autowired
     private SubwayMapService(IsHolidaysService isHolidaysService) {
         this.isHolidaysService = isHolidaysService;
+
+        validSet.add("h0530");
+        validSet.add("h0600");
+        validSet.add("h0630");
+        validSet.add("h0700");
+        validSet.add("h0730");
+        validSet.add("h0800");
+        validSet.add("h0830");
+        validSet.add("h0900");
+        validSet.add("h0930");
+        validSet.add("h1000");
+        validSet.add("h1030");
+        validSet.add("h1100");
+        validSet.add("h1130");
+        validSet.add("h1200");
+        validSet.add("h1230");
+        validSet.add("h1300");
+        validSet.add("h1330");
+        validSet.add("h1400");
+        validSet.add("h1430");
+        validSet.add("h1500");
+        validSet.add("h1530");
+        validSet.add("h1600");
+        validSet.add("h1630");
+        validSet.add("h1700");
+        validSet.add("h1730");
+        validSet.add("h1800");
+        validSet.add("h1830");
+        validSet.add("h1900");
+        validSet.add("h1930");
+        validSet.add("h2000");
+        validSet.add("h2030");
+        validSet.add("h2100");
+        validSet.add("h2130");
+        validSet.add("h2200");
+        validSet.add("h2230");
+        validSet.add("h2300");
+        validSet.add("h2330");
+        validSet.add("h0000");
+        validSet.add("h0030");
     }
 
     public List<SubwayMapInfo> getSubwayMapInfo(String stationName){
@@ -27,6 +69,9 @@ public class SubwayMapService {
         String dayType = getCurrentDayType();
         return subwayMapMapper.getSubwayMapInfo(stationName, dayType, currentColumn);
     }
+
+    private final Set<String> validSet = new HashSet<>();
+
 
     //현재 시간을 LocalTime으로 받아와서 h0000 형식으로 반환하는 메소드
     public String getCurrentTimeColumn(){
@@ -48,7 +93,7 @@ public class SubwayMapService {
             return "h0030"; // h0100 ~ h0500은 리턴하지 않음
         }
 
-        // h0000 ~ h2350의 유효한 시간 반환
+        // h0000 ~ h2330의 유효한 시간 반환
         if (hour >= 0 && hour <= 23) {
             return String.format("h%02d%02d", hour, minute);
         }
@@ -61,6 +106,17 @@ public class SubwayMapService {
         String result = isHolidaysService.classifyDate(now);
 //        System.out.println("subwayMapService getCurrentDayType : " + result);
         return result;
+    }
+
+    // h0000 꼴의 입력이 올바른 입력인지 체크해주는 함수
+    public String isValidTimeFormat(String input){
+        if(validSet.contains(input)){
+            return input;
+        }
+        else {
+            System.err.println("at SubWayMap Service, isValidTimeFormate : Wrong input");
+            return "h0030";
+        }
     }
 
 }
