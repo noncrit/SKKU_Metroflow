3c1e478d5b28a68a32667efdcbc6b6c815853915
seok_soldesk <kimksj09996@gmail.com>
2024-10-11 12:39:12 +0900

    FEAT : 마우스 커서 구현중

--- src/main/resources/static/js/subwayMap/mouseEvent.js
+++ src/main/resources/static/js/subwayMap/mouseEvent.js
@@ -1,7 +1,8 @@
-const subwayMap = document.getElementsByClassName('subwayMap');
+const subwayMap = document.getElementsByClassName('subwayMap')[0];
 
 subwayMap.addEventListener('mouseenter', () => {
-    subwayMap.style.cursor = 'url(path/to/your/cursor-image.png), auto';
+    console.log("mouseEnter ON");
+    subwayMap.style.cursor = 'url(/images/mouse/cursor_convert.cur), auto';
 });
 
 subwayMap.addEventListener('mouseleave', () => {
